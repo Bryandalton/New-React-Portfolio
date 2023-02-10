9b20b86810546cc2864c17557513642ddf26784e
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2023-02-10 14:01:05 -0500

    initial commit

---
+++ bryan-dalton-portfolio/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+     <div id="header">
+    <h1>Bryan Dalton /&gt; </h1>
+    <h4>&lt; Web Design By:</h4>
+    </div>
+  <div id="menu">
+  <div id="menu-items">
+    {/* <div className="menu-item">Home</div> */}
+    <div className="menu-item">Portfolio</div>
+    <div className="menu-item">About Me</div>
+    <div className="menu-item">Contact</div>
+  </div>
+  <div id="menu-background-pattern"></div>
+  <div id="menu-background-image"></div>
+</div>
+</>
+  );
+}
+
+export default App;
