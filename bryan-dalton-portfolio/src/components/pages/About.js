78c53b9c05a9eedac9c9c605fcf2cd6004045726
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2023-03-10 12:50:03 -0500

    about info

--- bryan-dalton-portfolio/src/components/pages/About.js
+++ bryan-dalton-portfolio/src/components/pages/About.js
@@ -9,10 +9,7 @@ function About() {
         <div className="aboutCard">
           <h3>About Me:</h3>
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit
-            dolores autem praesentium dolorum officiis? Iusto velit, ab sapiente
-            hic dolorum blanditiis molestias iste fugiat placeat delectus maxime
-            expedita mollitia nobis.
+            Graduate of the Uiversity of Richmond Fullstack Flex-coding Bootcamp looking to break into the field of web development. Experience with analytical thinking, problem solving, project management, strong team skills, and excellent written and verbal communication skills.
           </p>
         </div>
       </div>
