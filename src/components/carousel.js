bb9f51b8ae2249640aff37375ceae77cc44c6854
Bryan Dalton <bryantdalton19@gmail.com>
2024-04-22 13:05:18 -0400

    carousel component

---
+++ src/components/carousel.js
@@ -0,0 +1,19 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+function carousel () {
+    <NavBar/>
+    return (
+    <div className="carousel-container">
+        <button className="handle left-handle">&#8249;</button>
+        <div className="slider">
+
+
+        </div>
+        <button className="handle right-handle">&8250;</button>
+
+    </div>
+    )
+}
+
+export default carousel
