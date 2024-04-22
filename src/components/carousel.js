19d8192b6a91a2fb9f1272ec6fb97757841f9a1a
Bryan Dalton <bryantdalton19@gmail.com>
2024-04-22 17:07:55 -0400

    state slider working

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,78 +1,92 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import Project from "./project";
 import "./carousel.css";
 
-
 const projects = [
-    {
-      img: "./images/mfhf.PNG",
-      name: "My Favorite Historical Figures",
-      link: "https://bryandalton.github.io/my_favorite_historical_figure/",
-      github: "https://github.com/Bryandalton/my_favorite_historical_figure",
-      description:
-        "React site that offers a list of historical figures, shows a map of a location important to the active figure, and has a collapsible profile for the active figure.",
-    },
-    {
-      img: "./images/cryptsndbrd.PNG",
-      name: "Cryptid Sound Board",
-      link: "https://bryandalton.github.io/cryptidsoundboard/",
-      github: "https://github.com/Bryandalton/cryptidsoundboard",
-      description:
-        "A fun and responsive site to see what sounds cryptids might make!",
-    },
-    {
-      img: "./images/kandy-karma.JPG",
-      name: "Kandy Karma",
-      link: "https://bryandalton.github.io/kandykarmafrontend/",
-      github: "https://github.com/Bryandalton/kandykarmafrontend",
-      description: "Mock market research site for candy.",
-    },
-    {
-      img: "./images/WeatherServiceScreenshot.jpg",
-      name: "Weather Service",
-      link: "https://bryandalton.github.io/Weather-service/",
-      github: "https://github.com/Bryandalton/Weather-service",
-      description:
-        "Displays the weather, five day forecast, and remembers last city selected.",
-    },
-    {
-      img: "./images/Bigfootbutton.jpg",
-      name: "Bigfoot Button",
-      link: "https://codepen.io/Iwritebadcode/full/mdXdxpz",
-      github: "https://codepen.io/Iwritebadcode/pen/mdXdxpz",
-      description: 'Neon button that plays a "bigfoot" noise when clicked.',
-    },
-    {
-      img: "./images/Paint Your Mood.png",
-      name: "Paint your mood",
-      link: "https://bryandalton.github.io/Paint-your-mood/",
-      github: "https://github.com/Bryandalton/Paint-your-mood",
-      description:
-        "Website to display artwork from Chicago and Harvard art APIs.",
-    },
-  ];
+  {
+    img: "./images/mfhf.PNG",
+    name: "My Favorite Historical Figures",
+    link: "https://bryandalton.github.io/my_favorite_historical_figure/",
+    github: "https://github.com/Bryandalton/my_favorite_historical_figure",
+    description:
+      "React site that offers a list of historical figures, shows a map of a location important to the active figure, and has a collapsible profile for the active figure.",
+  },
+  {
+    img: "./images/cryptsndbrd.PNG",
+    name: "Cryptid Sound Board",
+    link: "https://bryandalton.github.io/cryptidsoundboard/",
+    github: "https://github.com/Bryandalton/cryptidsoundboard",
+    description:
+      "A fun and responsive site to see what sounds cryptids might make!",
+  },
+  {
+    img: "./images/kandy-karma.JPG",
+    name: "Kandy Karma",
+    link: "https://bryandalton.github.io/kandykarmafrontend/",
+    github: "https://github.com/Bryandalton/kandykarmafrontend",
+    description: "Mock market research site for candy.",
+  },
+  {
+    img: "./images/WeatherServiceScreenshot.jpg",
+    name: "Weather Service",
+    link: "https://bryandalton.github.io/Weather-service/",
+    github: "https://github.com/Bryandalton/Weather-service",
+    description:
+      "Displays the weather, five day forecast, and remembers last city selected.",
+  },
+  {
+    img: "./images/Bigfootbutton.jpg",
+    name: "Bigfoot Button",
+    link: "https://codepen.io/Iwritebadcode/full/mdXdxpz",
+    github: "https://codepen.io/Iwritebadcode/pen/mdXdxpz",
+    description: 'Neon button that plays a "bigfoot" noise when clicked.',
+  },
+  {
+    img: "./images/Paint Your Mood.png",
+    name: "Paint your mood",
+    link: "https://bryandalton.github.io/Paint-your-mood/",
+    github: "https://github.com/Bryandalton/Paint-your-mood",
+    description:
+      "Website to display artwork from Chicago and Harvard art APIs.",
+  },
+];
 
-export default function carousel () {
-    <NavBar/>
-    return (
-    <div className="carousel-container">
-        <button className="handle left-handle">&#8249;</button>
+export default function Carousel() {
+  const [sliderIndex, setSliderIndex] = useState(0);
+  useEffect(() => {
+    document.documentElement.style.setProperty("--slider-index", sliderIndex);
+  }, [sliderIndex]);
+
+  const onNextClick = () => {
+    setSliderIndex(prev => prev + 1);
+  };
+  const onPrevClick = () => {
+    setSliderIndex(prev => prev - 1);
+  };
+  return (
+    <>
+      <NavBar />
+      <div className="carousel-container">
+        <button onClick={onPrevClick} className="handle left-handle">&#8249;</button>
+        {/* <div className="slider" style={{'--slider-index': `${sliderIndex}`}}> */}
         <div className="slider">
-            {projects.map((project) => {
+          {projects.map((project) => {
             return (
-                <Project
-                    link={project.link}
-                    img={project.img}
-                    name={project.name}
-                    github={project.github}
-                    description={project.description}
-                />
-                );
-            })}
+              <Project
+                link={project.link}
+                img={project.img}
+                name={project.name}
+                github={project.github}
+                description={project.description}
+              />
+            );
+          })}
         </div>
-        <button className="handle right-handle">&#8250;</button>
-
-    </div>
-    )
+        <button onClick={onNextClick} className="handle right-handle">
+          &#8250;
+        </button>
+      </div>
+    </>
+  );
 }
